ff3045da559a0abd921e3db0f4c702d905c50426
ViniciusMazinetti <vinicius_mazinetti@hotmail.com>
2021-09-08 16:59:12 -0300

    CRUD do client arrumado

--- src/main/java/br/com/fiap/dimdim/controller/ClientController.java
+++ src/main/java/br/com/fiap/dimdim/controller/ClientController.java
@@ -5,16 +5,18 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.web.servlet.view.RedirectView;
 
+import br.com.fiap.dimdim.model.Bill;
 import br.com.fiap.dimdim.model.Client;
 import br.com.fiap.dimdim.repository.ClientRepository;
 
@@ -39,42 +41,28 @@ public class ClientController {
     }
     
     @PostMapping("/save")
-    public String save(Client client, BindingResult result, RedirectAttributes redirect) {
+    public String save(@ModelAttribute("client") Client client, BindingResult result, RedirectAttributes redirect) {
         if (result.hasErrors()) {
             return "client/form";
         }
         
-        Optional<Client> updateClient = clientRepository.findById(client.getId());
-        if (updateClient.isEmpty()) {
-        	clientRepository.save(client);
-            redirect.addFlashAttribute("msg", "Cadastrado");
-            return "redirect:save";
-        }
-        System.out.println("Entrei aqui");
-        updateClient.get().setId(client.getId());
-        updateClient.get().setName(client.getName());
-        updateClient.get().setEmail(client.getEmail());
-        updateClient.get().setCpf(client.getCpf());
-        clientRepository.save(updateClient.get());
-        redirect.addFlashAttribute("msg", "Atualizado");
+        clientRepository.save(client);
+        redirect.addFlashAttribute("msg", "Cadastrado!!!");
         return "redirect:save";
         
     }
     
-    @RequestMapping("/update/{id}")
-	public ModelAndView update(@PathVariable Long id) {
-    	Optional<Client> client = clientRepository.findById(id);
+    @GetMapping("/update/{id}")
+	public String update(@PathVariable Long id, Model model) {
+    	Client client = clientRepository.findById(id).get();
     	
-    	if(client.isPresent()) {	
-    		ModelAndView modelAndView = new ModelAndView("client/form");
-    		modelAndView.addObject("client", client.get());
-    		return modelAndView;
-    	}
-    	ModelAndView modelAndView = new ModelAndView("client/list");
-		return modelAndView;
+    	model.addAttribute("client", client);
+    	
+		return "client/updateForm";
 	}
-	
-	@RequestMapping("/delete/{id}")
+    
+    
+	@GetMapping("/delete/{id}")
 	public RedirectView delete(@PathVariable(value = "id") Long id, RedirectAttributes redirect) {
 		Optional<Client> client = clientRepository.findById(id);
 		System.out.println("Entrei Aqui");
