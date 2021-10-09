198e4c2354909f44c1fce2fc489d82a0f5367941
ViniciusMazinetti <vinicius_mazinetti@hotmail.com>
2021-10-09 10:55:45 -0300

    Mudanças

--- src/main/java/br/com/fiap/dimdim/DimdimApplication.java
+++ src/main/java/br/com/fiap/dimdim/DimdimApplication.java
@@ -2,9 +2,16 @@ package br.com.fiap.dimdim;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DimdimApplication {
+public class DimdimApplication extends SpringBootServletInitializer{
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(DimdimApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DimdimApplication.class, args);
